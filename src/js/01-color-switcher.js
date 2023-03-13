559982859b291a639333afece0b4dbecc145325d
ssulphurr <ssulphurr@gmail.com>
2023-03-14 00:50:28 +0200

    adds task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const refs = {
+  body: document.querySelector('body'),
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
 
+let timerId = null;
+
+function changeColor() {
+  refs.startBtn.removeEventListener('click', changeColor);
+
+  // початкова зміна кольору, щоб не було затримки в 1 с.
+  refs.body.style.backgroundColor = getRandomHexColor();
+
+  timerId = setInterval(() => {
+    color = getRandomHexColor();
+    refs.body.style.backgroundColor = color;
+  }, 1000);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function stopChangeColor() {
+  clearInterval(timerId);
+  refs.startBtn.addEventListener('click', changeColor);
+}
+
+refs.startBtn.addEventListener('click', changeColor);
+refs.stopBtn.addEventListener('click', stopChangeColor);
